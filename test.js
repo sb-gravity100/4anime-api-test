4874c49ccac85fbc2e2dedc096a7ccdaf4b72e95
sb-gravity100 <sbducao0@gmail.com>
2021-04-20 05:05:16 +0800

    After 4fcfa55

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ const { FourAnime } = require('./index.js');
 
 const Anime = new FourAnime()
 
+Anime.on('loaded', console.log)
+Anime.on('error', console.log)
+
 Anime.term('jujutsu kaisen', res => {
    if (res) {
       console.log('Search is fine.')
