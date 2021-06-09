96473688471b223e66495e5cea41c586e9ade096
Sb <sbducao0@gmail.com>
2021-06-09 21:19:09 +0000

    Tests Done

--- test/index.js
+++ test/index.js
@@ -1,20 +1,16 @@
-const FourAnime = require('../index');
-
-const Anime = new FourAnime({
-   catch: true,
-});
-
-Anime.on('loaded', console.log);
+const FourAnime = require('../dist');
 
 (async () => {
    try {
       const fs = require('fs');
-      const search = await Anime.term('shingeki no kyojin');
-      console.log('Search done')
-      const anime = await search[0].getAnime()
-      console.log(anime)
-      fs.writeFileSync('./snk1', JSON.stringify(anime))
+      const search = await FourAnime.term('shingeki no kyojin');
+      console.log('Search done');
+      const anime = await search[0].getAnime();
+      console.log(anime.get());
    } catch (e) {
+      if (e.isAxiosError) {
+         e = e.toJSON();
+      }
       console.log(e);
    }
 })();
